3010d03183d368222cd37bc0ed4707c8843f79d5
wangkai <1976299863@qq.com>
2020-02-09 21:19:32 +0800

    修改了pom文件

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AaaApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AaaApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
