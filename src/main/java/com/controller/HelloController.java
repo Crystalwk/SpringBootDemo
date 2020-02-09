f05a4190e32df84bb23d3013681f45a10360b939
wangkai <1976299863@qq.com>
2020-02-09 22:22:19 +0800

    学完了第三章web之旅

---
+++ src/main/java/com/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(ModelMap modelMap){
+        modelMap.addAttribute("msg","Hello Crystal!");
+        return "index" ;
+    }
+}
